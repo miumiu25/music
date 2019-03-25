672731260cf3e0982e1c6a9c8fed2aa0efe761cd
miumiu25 <837830133@qq.com>
2019-03-25 20:20:56 +0800

    ajax

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-console.log($('.wrapper'));
+function getData(url){
+    $.ajax({
+        type:"GET",
+        url:url,
+        success:function(data){
+            console.log(1);
+        },
+        error:function(){
+            console.log("error");
+        }
+    })
+}
+
+
+
+getData('../mock/data.json');
